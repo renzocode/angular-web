79dacfe5ab9dfb27c305ae0cbb8e11b637ea7501
renzocode <rsalcedop@uni.pe>
2019-11-25 20:18:44 -0500

    compoment table

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewcomponentComponent } from './viewcomponent/viewcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewcomponentComponent
   ],
   imports: [
     BrowserModule,
